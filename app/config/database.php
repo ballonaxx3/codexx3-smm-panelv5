2a7812c265ffb2566283c33e6e34d27625719b48
ballonaxx3 <jeremyballona25@gmail.com>
2026-04-30 01:14:55 -0600

    Use PDO DSN and remove unsupported legacy DB options

--- app/config/database.php
+++ app/config/database.php
@@ -5,9 +5,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db_port = defined('DB_PORT') ? DB_PORT : 3306;
+$dsn = 'mysql:host='.DB_HOST.';port='.(int) $db_port.';dbname='.DB_NAME.';charset=utf8mb4';
 
 $db['default'] = array(
-    'dsn'      => '',
+    'dsn'      => $dsn,
     'hostname' => DB_HOST,
     'port'     => (int) $db_port,
     'username' => DB_USER,
@@ -17,15 +18,12 @@ $db['default'] = array(
     'subdriver' => 'mysql',
     'dbprefix' => '',
     'pconnect' => FALSE,
-    'db_debug' => TRUE,
+    'db_debug' => FALSE,
     'cache_on' => FALSE,
     'cachedir' => '',
     'char_set' => 'utf8mb4',
     'dbcollat' => 'utf8mb4_general_ci',
     'swap_pre' => '',
     'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
     'save_queries' => TRUE
 );
